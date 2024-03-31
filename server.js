1b9a1f4a0ee1ddf0a06c7bc085d0f11358869485
meni-kr <meni.kramer@gmail.com>
2024-03-31 13:01:31 +0300

    Change to new backend

--- server.js
+++ server.js
@@ -1,99 +1,57 @@
-import express from "express";
-import cors from "cors";
-import cookieParser from "cookie-parser";
-import { toyService } from "./services/toy.service.js";
+import express  from 'express'
+import cookieParser from 'cookie-parser'
+import cors  from 'cors'
+import path, { dirname } from 'path'
+import { fileURLToPath } from 'url'
 
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
+import { logger } from './services/logger.service.js'
+logger.info('server.js loaded...')
 
 const app = express()
 
 // Express App Config
-app.use(express.json())
 app.use(cookieParser())
+app.use(express.json())
 app.use(express.static('public'))
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('public'));
+    // Express serve static files on production environment
+    app.use(express.static(path.resolve(__dirname, 'public')))
+    console.log('__dirname: ', __dirname)
 } else {
+    // Configuring CORS
     const corsOptions = {
-        origin: [
-            'http://127.0.0.1:3000',
-            'http://localhost:3000',
-            'http://localhost:5173',
-            'http://127.0.0.1:5173',
-        ],
+        // Make sure origin contains the url your frontend is running on
+        origin: ['http://127.0.0.1:5173', 'http://localhost:5173','http://127.0.0.1:3000', 'http://localhost:3000'],
         credentials: true
-    };
-    app.use(cors(corsOptions));
+    }
+    app.use(cors(corsOptions))
 }
 
-app.get('/api/toy', (req, res) => {
-    const { filterBy = {}, sort = {} } = req.query.params
-    // const filterBy ={}
-    // const sort ={}
-    // console.log("req.query.params:", req.query.params)
+import { authRoutes } from './api/auth/auth.routes.js'
+import { userRoutes } from './api/user/user.routes.js'
 
-    toyService.query(filterBy, sort)
-        .then(toys => {
-            res.send(toys)
-        })
-        .catch(err => {
-            console.log('Had issues getting toys', err);
-            res.status(400).send({ msg: 'Had issues getting toys' })
-        })
+import { toyRoutes } from './api/toy/toy.routes.js'
+
+// routes
+app.use('/api/auth', authRoutes)
+app.use('/api/user', userRoutes)
+
+app.use('/api/toy', toyRoutes)
+
+// Make every unmatched server-side-route fall back to index.html
+// So when requesting http://localhost:3030/index.html/toy/123 it will still respond with
+// our SPA (single page app) (the index.html file) and allow vue-router to take it from there
+
+app.get('/**', (req, res) => {
+    res.sendFile(path.resolve('public/index.html'))
 })
 
-app.get('/api/toy/:id', (req, res) => {
-    const toyId = req.params.id
-    toyService.getById(toyId)
-        .then(toy => {
-            res.send(toy)
-        })
-        .catch(err => {
-            console.log('Had issues getting toy', err);
-            res.status(400).send({ msg: 'Had issues getting toy' })
-        })
-})
+const port = process.env.PORT || 3030
 
-app.delete('/api/toy/:id', (req, res) => {
-    const toyId = req.params.id
-    toyService.remove(toyId)
-        .then(() => {
-            res.end('Done!')
-        })
-        .catch(err => {
-            console.log('Had issues deleting toy', err);
-            res.status(400).send({ msg: 'Had issues deleteing toy' })
-        })
-})
-
-/// Add new toy
-app.post('/api/toy', (req, res) => {
-    const toy = req.body
-    toyService.save(toy)
-        .then(savedToy => {
-            res.send(savedToy)
-        })
-        .catch(err => {
-            console.log('Had issues adding toy', err);
-            res.status(400).send({ msg: 'Had issues adding toy' })
-        })
-})
-
-/// update toy
-app.put('/api/toy/:id', (req, res) => {
-    const toy = req.body
-    toyService.save(toy)
-        .then(savedToy => {
-            res.send(savedToy)
-        })
-        .catch(err => {
-            console.log('Had issues updating toy', err);
-            res.status(400).send({ msg: 'Had issues updating toy' })
-        })
-})
-
-const port = 3030
 app.listen(port, () => {
-    console.log('Server is up and listening to', port);
+    logger.info('Server is running on port: ' + port)
 })
