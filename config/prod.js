5fcecd984f36dfd98cb587fa8576dd34eecb82fe
meni-kr <meni.kramer@gmail.com>
2024-04-02 02:07:08 +0300

    Change url for db

--- config/prod.js
+++ config/prod.js
@@ -1,4 +1,5 @@
 export default {
-  dbURL: process.env.MONGO_URL || 'mongodb+srv://theUser:thePass@cluster0-klgzh.mongodb.net/test?retryWrites=true&w=majority',
+  // dbURL: process.env.MONGO_URL || 'mongodb+srv://theUser:thePass@cluster0-klgzh.mongodb.net/test?retryWrites=true&w=majority',
+  dbURL: process.env.MONGO_URL || 'mongodb+srv://meni:meni123@cluster0.rcfrr9v.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
   dbName : process.env.DB_NAME || 'toyDB'
 }
